7b3e51ac596b9a37babcd5a4e06c156083bece17
Bentouati Mehdi <mehdi.bentouati76@gmail.com>
2024-01-13 21:11:34 +0100

    feat: adding targets folder and config

--- src/arch/x86/boot/boot.s
+++ src/arch/x86/boot/boot.s
@@ -1,7 +1,8 @@
-.set ALIGN, 1 << 0
-.set MEMINFO, 1 << 1
+.set ALIGN, 1<<0
+.set MEMINFO, 1<<1
 .set FLAGS, ALIGN | MEMINFO
-.set CHECKSUM, -(MAGIC+FLAGS)
+.set MAGIC, 0x1BADB002
+.set CHECKSUM, -(MAGIC + FLAGS)
 
 .section .multiboot
 .align 4
@@ -15,16 +16,19 @@
 _start:
 	mov $stack_top, %esp
 
-	; set up Paging 
-	; load GDT here before entering in C code
-	; set CPU to long mode (PM is already set by grub)
+	/* set up Paging 
+	 load GDT here before entering in C code
+	 set CPU to long mode (PM is already set by grub) */
 	call kmain
 
 	cli
 	hlt
+.size _start, . - _start
 
 .section .bss
 .align 16
 stack_bottom:
 .skip 16384
 stack_top:
+
+
