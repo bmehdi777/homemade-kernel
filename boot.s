5ac5b346f4e3a382e4c39268161040bf86ceddc6
Bentouati Mehdi <mehdi.bentouati76@gmail.com>
2024-01-13 18:40:53 +0100

    setting up boot

---
+++ boot.s
@@ -0,0 +1,30 @@
+.set ALIGN, 1 << 0
+.set MEMINFO, 1 << 1
+.set FLAGS, ALIGN | MEMINFO
+.set CHECKSUM, -(MAGIC+FLAGS)
+
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+.section .text
+.global _start
+.type _start, @function
+_start:
+	mov $stack_top, %esp
+
+	; set up Paging 
+	; load GDT here before entering in C code
+	; set CPU to long mode (PM is already set by grub)
+	call kmain
+
+	cli
+	hlt
+
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384
+stack_top:
